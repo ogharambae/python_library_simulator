a5979ef2e547041d32fbb81985b64282d6338f08
maxab <mandersonbaier@my.bcit.ca>
2022-03-05 11:54:39 -0800

    implement abstract classes: Item, Toys, stuffed Animals and Candy

--- Item.py
+++ Item.py
@@ -138,7 +138,6 @@ class StuffedAnimals(ABC, Item):
 class Candy(ABC, Item):
     """
     An abstract class which extends Item and represents Candy.
-
     """
 
     def __init__(self, name: str, description: str,
