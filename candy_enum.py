a33e9fbf0c678d44e60e6735061edd3d91043251
maxab <mandersonbaier@my.bcit.ca>
2022-03-05 16:55:34 -0800

    add docstring to candy cane enum

--- candy_enum.py
+++ candy_enum.py
@@ -2,12 +2,18 @@ from enum import Enum
 
 
 class ToffeeVariety(Enum):
+    """
+    Enum class which represents various Toffee Varieties.
+    """
 
     salt = "sea salt"
     reg = "regular"
 
 
 class CandyCaneVariety(Enum):
+    """
+    Enum class which represents various CandyCane Varieties.
+    """
 
     red = "red"
     green = "green"
