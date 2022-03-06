41e577dad34fcf78defa83491d6f5d9224eaa1ae
Hyerim <sinhyerim@gmail.com>
2022-03-06 13:10:42 -0800

    Implemented file_manager.py

--- file_manager.py
+++ file_manager.py
@@ -1,5 +1,31 @@
+import pandas as pd
+
+
 class FileManager:
     """
     Represent the File Manager which involves in file io operations
     """
-    pass
+
+    @staticmethod
+    def write_report():
+        """
+        Read Excel file and process data to list of dictonary
+        """
+
+        list_of_order = []
+
+        df = pd.read_excel(r"./orders.xlsx", index_col=0, na_values=['NA']).to_dict('records')
+
+        for column in df:
+            list_of_order.append(column)
+
+        return list_of_order
+
+
+def main():
+    FileManager.write_report()
+
+
+if __name__ == '__main__':
+    main()
+
