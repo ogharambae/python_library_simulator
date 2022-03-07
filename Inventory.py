b3dedd8387295c44fd2757b8a3c77c9fd25a8894
HyunBae Choi <hyunbaechoi93@gmail.com>
2022-03-06 18:47:59 -0800

    implement store_front_controller.py and Inventory.py

--- Inventory.py
+++ Inventory.py
@@ -2,4 +2,31 @@ class Inventory:
     """
     Represent the Inventory class of the store which contains the item stock information
     """
-    pass
+
+    def __init__(self):
+        self.item_stock = {}
+
+    def check_stock(self, order):
+        """
+        Returns True if there is stock of the specified order.
+
+        :return: True if there is stock of the specified order
+        """
+        item = order.item
+        if item.get_product_id() in self.item_stock:
+            if self.item_stock[item.get_product_id] > order.get_quantity():
+                return True
+        else:
+            self.item_stock[item.get_product_id] = 0
+
+    def restock(self, item):
+        """
+        Restocks an item by a count of 100.
+        """
+        self.item_stock[item.get_product_id()] += 100
+
+    def fulfill_order(self, order):
+        """
+        Reduces the amount of stock of a specified item by the specified amount.
+        """
+        self.item_stock[order.item.get_product_id()] -= order.get_quantity()
--- store_front_controller.py
+++ store_front_controller.py
@@ -1,5 +1,35 @@
+from Store import Store
+
+
 class StoreFrontController:
     """
-    Represent the Store Front Controller of the program
+    Represents the Store Front Controller of the program.
     """
-    pass
+    def __init__(self):
+        self.store = Store()
+
+    def import_order(self, orders):
+        """
+        Returns a list of updated orders.
+
+        :return: a list representing an updated list of orders
+        """
+        for order in orders:
+            if not order.order_success:
+                continue
+            if self.store.inventory.check_stock(order):
+                self.store.inventory.fulfill_order(order)
+                self.store.append_order(order)
+            else:
+                self.store.inventory.restock()
+                self.store.inventory.fulfill_order(order)
+                self.store.append_order(order)
+        return self.store.orders
+
+    def get_imported_orders(self):
+        """
+        Returns the imported list of orders from the Store.
+
+        :return: a list representing a list of orders
+        """
+        return self.store.orders
