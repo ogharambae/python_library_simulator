1d9b6e006856f478fd5c8f65b5814a0cd9a8b9ca
HyunBae Choi <hyunbaechoi93@gmail.com>
2022-03-06 17:56:00 -0800

    created get_quantity method

--- Store.py
+++ Store.py
@@ -1,5 +1,33 @@
+from Inventory import Inventory
+
+
 class Store:
     """
-    Represent the Store object of the program
+    Represents the Store object of the program.
     """
-    pass
+
+    def __init__(self):
+        self.inventory = Inventory()
+        self.orders = []
+
+    @property
+    def orders(self):
+        """
+        Returns the list of orders stored in the Store.
+
+        :return: a list representing a list of orders
+        """
+        return self.orders
+
+    @orders.setter
+    def orders(self, orders):
+        """
+        Sets the list of orders stored in the Store.
+        """
+        self.orders = orders
+
+    def append_order(self, order):
+        """
+        Adds a new order to the list of orders stored in Store.
+        """
+        self.orders.append(order)
